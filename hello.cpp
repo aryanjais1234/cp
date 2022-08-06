5fe740501a43ba597c060a6a5b34b28cda8771ba
aryanjais1234 <aryanjais1234@gmail.com>
2022-08-07 00:05:11 +0530

    new

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,5 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a;
-	cin>>a;
-	int b;
-    cin>>b;
-    cout<<"Hello";
-    return 0;
+    cout<<"Hello World";
 }
---
+++ vector.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+int main(){
+    vector<int>v(5,10);
+    int sum=0;
+    for(int x:v){
+        sum+=x;
+    }
+    cout<<sum<<endl;
+
+    vector<int>::iterator it;
+    for(it=v.begin();it<v.end();it++){
+        cout<<*(it)<<endl;
+    }
+
+
+}
